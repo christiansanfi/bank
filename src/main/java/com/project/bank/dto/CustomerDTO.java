6c319941802fdcf869bdfe537c9b07fa6ad76290
Christian_Sanfilippo <san.chri.cs02@gmail.com>
2024-10-30 11:13:28 +0100

    Adding conversion in CustomerDTO

--- src/main/java/com/project/bank/dto/CustomerDTO.java
+++ src/main/java/com/project/bank/dto/CustomerDTO.java
@@ -3,6 +3,7 @@ package com.project.bank.dto;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import com.project.bank.model.Customer;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,10 +11,30 @@ import lombok.Setter;
 @Setter
 
 public class CustomerDTO {
-    private UUID customerId;
+    private UUID id;
     private String name;
     private LocalDateTime birthDate;
     private String birthPlace;
     private String taxCode;
     private String address;
+
+    public CustomerDTO(Customer customer) {
+        this.id = customer.getId();
+        this.name = customer.getName();
+        this.birthDate = customer.getBirthDate();
+        this.birthPlace = customer.getBirthPlace();
+        this.taxCode = customer.getTaxCode();
+        this.address = customer.getAddress();
+    }
+
+    public Customer toEntity() {
+        Customer customer = new Customer();
+        customer.setId(this.id);
+        customer.setName(this.name);
+        customer.setBirthDate(this.birthDate);
+        customer.setBirthPlace(this.birthPlace);
+        customer.setTaxCode(this.taxCode);
+        customer.setAddress(this.address);
+        return customer;
+    }
 }
