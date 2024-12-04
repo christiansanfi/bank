592b280caba7864f434555b36ab244d60a1a5296
Christian_Sanfilippo <san.chri.cs02@gmail.com>
2024-12-04 10:20:36 +0100

    Removing unuseful annotations

--- src/main/java/com/project/bank/BankApplication.java
+++ src/main/java/com/project/bank/BankApplication.java
@@ -2,11 +2,9 @@ package com.project.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.project.bank.model")
 public class BankApplication {
 
     public static void main(String[] args) {
